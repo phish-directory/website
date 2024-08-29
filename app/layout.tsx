42807692d11e3439a9cfa34cebce27daaf4e5af2
Jasper Mayone <me@jaspermayone.com>
2024-08-29 13:03:53 -0400

    faviconnnn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
+import clsx from 'clsx'
 import { type Metadata } from 'next'
 import { Inter, Lexend } from 'next/font/google'
-import clsx from 'clsx'
+import Head from 'next/head'
 
-import "./globals.css";
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import './globals.css'
 
 export const metadata: Metadata = {
   title: {
@@ -41,11 +42,96 @@ export default function RootLayout({
         lexend.variable,
       )}
     >
-      <body className="flex flex-col min-h-full">
+      <Head>
+        <meta
+          name="description"
+          content="Stay safe from phishing attacks. Phish Directory is a community-driven database of phishing URLs."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Phish Directory</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-touch-icon-57x57.png?v=2"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-touch-icon-60x60.png?v=2"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-touch-icon-72x72.png?v=2"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-touch-icon-76x76.png?v=2"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-touch-icon-114x114.png?v=2"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-touch-icon-120x120.png?v=2"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-touch-icon-144x144.png?v=2"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-touch-icon-152x152.png?v=2"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon-180x180.png?v=2"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png?v=2"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png?v=2"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png?v=2"
+        />
+        <link rel="manifest" href="/site.webmanifest?v=2" />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg?v=2"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta
+          name="msapplication-TileImage"
+          content="/mstile-144x144.png?v=2"
+        />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <body className="flex min-h-full flex-col">
         <Header />
         {children}
         <Footer />
-        </body>
+      </body>
     </html>
   )
 }
